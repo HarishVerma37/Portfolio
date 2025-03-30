d3d83e2b81b6d99f4a6a04e9859cf4faa60e6a12
HarishVerma37 <145188009+HarishVerma37@users.noreply.github.com>
2025-03-30 04:35:19 -0700

    Update Contact.jsx

--- src/ContactComponent/Contact.jsx
+++ src/ContactComponent/Contact.jsx
@@ -1,15 +1,15 @@
 import "./Contact.css";
-export default function Contact({ icon, title, phone, link }) {
+
+export default function Contact({ icon, title, phone, link ,contactStyle}) {
     return (
-
-
-
-        <a href={link} className="contact">
-            <span>{icon}</span>
-            <span>{title}</span>
-            <span className="head">{phone}</span>
+         <a href={link} className="contact"  style={contactStyle}>
+            <div className="icon-container">
+                {icon}
+            </div>
+            <div>
+                <span className="title">{title}</span>
+                <a href={`tel:${phone}`} className="phone">{phone}</a>
+            </div>
         </a>
-
-
     );
 }
